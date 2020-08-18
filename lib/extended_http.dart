bc81db334644aaf478f49a006ef474e9d88e37c1
Ngoan Tran <ngoan98travinh@gmail.com>
2020-08-18 21:01:32 +0700

    add extended http

---
+++ lib/extended_http.dart
@@ -0,0 +1,80 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:http/http.dart';
+
+class ExtendedHttp extends BaseClient {
+  final Client _client;
+  Map<String, String> _headers;
+  Duration _timeout;
+  bool _networkFirst;
+
+  static final _instance = ExtendedHttp._internal(Client());
+
+  factory ExtendedHttp({
+    Duration timeout = const Duration(seconds: 60),
+    bool networkFirst = true,
+    Map<String, String> headers,
+  }) {
+    _instance._networkFirst = networkFirst;
+    _instance._timeout = timeout;
+    _instance._headers = headers;
+    return _instance;
+  }
+
+  ExtendedHttp._internal(this._client);
+
+  @override
+  Future<StreamedResponse> send(BaseRequest req) async {
+    if (_headers != null) req.headers.addAll(_headers);
+
+    if (req.method == "GET") {
+      return _networkFirst
+          ? await _networkFirstRequest(req)
+          : await _cacheFirstRequest(req);
+    } else {
+      return await _client.send(req).timeout(_timeout);
+    }
+  }
+
+  Future<StreamedResponse> _networkFirstRequest(BaseRequest req) async {
+    File file;
+    try {
+      file = await _downloadFile(req);
+    } catch (e) {
+      print(e);
+      file = await _readCachedFile(req);
+    }
+    return StreamedResponse(
+      file.readAsBytes().asStream(),
+      200,
+    );
+  }
+
+  Future<StreamedResponse> _cacheFirstRequest(BaseRequest req) async {
+    File file;
+    file = await _readCachedFile(req);
+    return StreamedResponse(
+      file.readAsBytes().asStream(),
+      200,
+    );
+  }
+
+  Future<File> _downloadFile(BaseRequest req) async {
+    final fileInfo = await DefaultCacheManager()
+        .downloadFile(
+          req.url.toString(),
+          authHeaders: req.headers,
+        )
+        .timeout(_timeout);
+    return fileInfo.file;
+  }
+
+  Future<File> _readCachedFile(BaseRequest req) async {
+    final file = await DefaultCacheManager().getSingleFile(
+      req.url.toString(),
+      headers: req.headers,
+    );
+    return file;
+  }
+}
