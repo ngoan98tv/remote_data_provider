e98f328ff808e069798d973d20245f314c61e500
Ngoan Tran <ngoan98travinh@gmail.com>
2022-10-04 22:48:47 +0700

    use hive for caching and add retry

--- lib/extended_http.dart
+++ lib/extended_http.dart
@@ -1,24 +1,68 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
-import 'package:flutter/material.dart';
-import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:flutter/foundation.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:http/http.dart';
+import 'package:http/retry.dart';
 
 /// Extend from `BaseClient`, adding caching and timeout features.
 class ExtendedHttp extends BaseClient {
   final Client _client;
   Map<String, String> _headers = {};
-  Duration _timeout = const Duration(seconds: 60);
-  bool _networkFirst = true;
+  Duration _timeout = const Duration(seconds: 5);
+  Duration _cacheAge = const Duration(seconds: 60);
+  bool _disableCache = false;
   String _baseURL = '';
+  Future<void> Function()? onUnauthorized;
 
-  static final _instance = ExtendedHttp._internal(Client());
+  Box<String>? _httpCacheBody;
+  Box<String>? _httpCacheHeader;
+
+  static final _instance = ExtendedHttp._internal(
+    RetryClient(
+      Client(),
+      when: _shouldRetry,
+      whenError: (e, stack) => e is TimeoutException || e is SocketException,
+      onRetry: _beforeRetry,
+    ),
+  );
 
   factory ExtendedHttp() {
+    if (_instance._httpCacheBody == null ||
+        _instance._httpCacheHeader == null) {
+      _instance.init();
+    }
     return _instance;
   }
 
   ExtendedHttp._internal(this._client);
+
+  static bool _shouldRetry(BaseResponse res) {
+    if ([401, 503].contains(res.statusCode)) {
+      return true;
+    }
+    return false;
+  }
+
+  static Future<void> _beforeRetry(
+    BaseRequest req,
+    BaseResponse? res,
+    int retryCount,
+  ) async {
+    _instance._log("Retry ($retryCount) ${req.method} ${req.url}");
+    _instance._log("Headers ${req.headers}");
+    if (res?.statusCode == 401 && _instance.onUnauthorized != null) {
+      await _instance.onUnauthorized!();
+      req.headers.addAll(_instance._headers);
+    }
+  }
+
+  Future<void> init() async {
+    await Hive.initFlutter();
+    _httpCacheBody = await Hive.openBox<String>('httpCacheBody');
+    _httpCacheHeader = await Hive.openBox<String>('httpCacheHeader');
+  }
 
   /// Override `ExtendedHttp` config
   ///
@@ -30,19 +74,17 @@ class ExtendedHttp extends BaseClient {
   ///
   /// `headers` -- Custom request headers.
   void config({
-    Duration timeout = const Duration(seconds: 60),
-    bool networkFirst = true,
-    Map<String, String>? headers,
     String? baseURL,
+    Duration? timeout,
+    bool? disableCache,
+    Duration? cacheAge,
+    Map<String, String>? headers,
   }) {
-    _networkFirst = networkFirst;
-    _timeout = timeout;
-    if (headers != null) {
-      _headers = headers;
-    }
-    if (baseURL != null) {
-      _baseURL = baseURL;
-    }
+    _timeout = timeout ?? _timeout;
+    _headers = headers ?? _headers;
+    _baseURL = baseURL ?? _baseURL;
+    _cacheAge = cacheAge ?? _cacheAge;
+    _disableCache = disableCache ?? _disableCache;
   }
 
   Uri createURI(String path, {Map<String, String>? params}) {
@@ -60,53 +102,100 @@ class ExtendedHttp extends BaseClient {
   Future<StreamedResponse> send(BaseRequest request) async {
     request.headers.addAll(_headers);
 
-    if (request.method == "GET") {
-      return _networkFirst
-          ? await _networkFirstRequest(request)
-          : await _cacheFirstRequest(request);
-    } else {
-      return await _client.send(request).timeout(_timeout);
+    _log("${request.method} ${request.url}");
+
+    if (_disableCache || request.method != "GET") {
+      return _client.send(request).timeout(_timeout);
     }
-  }
 
-  Future<StreamedResponse> _networkFirstRequest(BaseRequest req) async {
-    File file;
-    try {
-      file = await _downloadFile(req);
-    } catch (e) {
-      debugPrint(e.toString());
-      file = await _readCachedFile(req);
+    _log("Read from cache");
+    final cachedResponse = _responseFromCache(request.url);
+    if (cachedResponse != null) {
+      return cachedResponse;
     }
+
+    _log("Cache empty");
+    _log("Send request");
+
+    final response = await _client.send(request).timeout(_timeout);
+
+    if (response.statusCode != 200) {
+      return response;
+    }
+
+    _log("Write to cache");
+    final bodyString = await response.stream.bytesToString();
+    await _cacheResponse(request.url, bodyString, response.headers);
+
     return StreamedResponse(
-      file.readAsBytes().asStream(),
+      Stream.value(utf8.encode(bodyString)),
       200,
+      headers: response.headers,
+      reasonPhrase: response.reasonPhrase,
+      contentLength: response.contentLength,
+      request: response.request,
     );
   }
 
-  Future<StreamedResponse> _cacheFirstRequest(BaseRequest req) async {
-    File file;
-    file = await _readCachedFile(req);
+  Future<void> _cacheResponse(
+    Uri uri,
+    String bodyString,
+    Map<String, String> headers,
+  ) async {
+    final cacheKey = uri.toString();
+
+    await _httpCacheBody?.put(
+      cacheKey,
+      bodyString,
+    );
+
+    final validTo = DateTime.now().add(_cacheAge).toIso8601String();
+    headers['cache-valid-to'] = validTo;
+
+    final headerString = jsonEncode(headers);
+
+    await _httpCacheHeader?.put(
+      cacheKey,
+      headerString,
+    );
+  }
+
+  StreamedResponse? _responseFromCache(
+    Uri uri, {
+    bool ignoreValidDate = false,
+  }) {
+    final cacheKey = uri.toString();
+    final bodyString = _httpCacheBody?.get(cacheKey);
+    final headerString = _httpCacheHeader?.get(cacheKey);
+
+    if (bodyString == null || bodyString.isEmpty) {
+      return null;
+    }
+
+    final headerMap = jsonDecode(headerString ?? "{}") as Map<String, dynamic>;
+    final headers = headerMap.map((key, value) => MapEntry(key, "$value"));
+
+    if (!ignoreValidDate) {
+      if (headers['cache-valid-to'] == null) {
+        return null;
+      }
+
+      final validDate = DateTime.parse(headers['cache-valid-to']!);
+
+      if (validDate.isBefore(DateTime.now())) {
+        return null;
+      }
+    }
+
     return StreamedResponse(
-      file.readAsBytes().asStream(),
-      200,
+      Stream.value(utf8.encode(bodyString)),
+      203,
+      headers: headers,
+      reasonPhrase: "Cached Response",
     );
   }
 
-  Future<File> _downloadFile(BaseRequest req) async {
-    final fileInfo = await DefaultCacheManager()
-        .downloadFile(
-          req.url.toString(),
-          authHeaders: req.headers,
-        )
-        .timeout(_timeout);
-    return fileInfo.file;
-  }
-
-  Future<File> _readCachedFile(BaseRequest req) async {
-    final file = await DefaultCacheManager().getSingleFile(
-      req.url.toString(),
-      headers: req.headers,
-    );
-    return file;
+  void _log(String message) {
+    debugPrint("ExtendedHttp: $message");
   }
 }
